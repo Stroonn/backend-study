f8727b453421b549718d9a1e55db0a928de6e512
Stroonn <matheus.gato.gt98@gmail.com>
2026-02-16 13:46:49 -0300

    refactor main function to streamline user and product initialization

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from app.services.user_services import UserService
 from app.services.product_services import ProductService
 from app.utils.helpers import format_price
+from app.models.user import User
+from app.models.product import Product
 
 def main():
-    service = UserService()
-    user = service.create_user("Matheus", "matheus@gmail.com")
-    print(user)
-
-    p_service = ProductService()
-    try:
-        product = p_service.create_product("Glass", "Blue glass", 10, 50)
-    except ValueError as e:
-        print(f"Error: {e}")
-    product.price = format_price(product.price)
-    print(product)
+    #service = UserService()
+    #u1 = service.create_user("Matheus", "matheus@gmail.com")
+    #print(u1)
+    #u2 =  service.create_user("Gabriela", "gabriela@gmail.com")
     
+    #p_service = ProductService()
+    #try:
+    #    product = p_service.create_product("Glass", "Blue glass", 10, 50)
+    #except ValueError as e:
+    #    print(f"Error: {e}")
+    #product.price = format_price(product.price)
+    #print(product)
+
+    u1 = User("Matheus", "matheus@gmail.com")
+    u2 = User("Matheus", "matheus@gmail.com")
+
+    print(u1 == u2)
+
+    p1 = Product("Glass", "Blue glass", 10, 50)
+    #p1.price = format_price(p1.price)
+    #print(p1)
+
 if __name__ == "__main__":
     main()
