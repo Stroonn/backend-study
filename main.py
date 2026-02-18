a886e99ded0e5ef0ed734e58f7b00c7fc8e8b8ad
Stroonn <matheus.gato.gt98@gmail.com>
2026-02-18 18:32:00 -0300

    refactor main function to handle product creation errors with custom ProductError

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app.services.product_services import ProductService
 from app.utils.helpers import format_price
 from app.models.user import User
 from app.models.product import Product
+from app.errors.product_errors import ProductError
 
 def main():
     #service = UserService()
@@ -10,20 +11,20 @@ def main():
     #print(u1)
     #u2 =  service.create_user("Gabriela", "gabriela@gmail.com")
     
-    #p_service = ProductService()
-    #try:
-    #    product = p_service.create_product("Glass", "Blue glass", 10, 50)
-    #except ValueError as e:
-    #    print(f"Error: {e}")
-    #product.price = format_price(product.price)
-    #print(product)
+    p_service = ProductService()
+    try:
+        product = p_service.create_product("Glass", "Blue glass", 10, -50)
+    except ProductError as e:
+        print(f"Error: {e}")
+    product.price = format_price(product.price)
+    print(product)
 
-    u1 = User("Matheus", "matheus@gmail.com")
-    u2 = User("Matheus", "matheus@gmail.com")
+    #u1 = User("Matheus", "matheus@gmail.com")
+    #u2 = User("Matheus", "matheus@gmail.com")
 
-    print(u1 == u2)
+    #print(u1 == u2)
 
-    p1 = Product("Glass", "Blue glass", 10, 50)
+    #p1 = Product("Glass", "Blue glass", 10, 50)
     #p1.price = format_price(p1.price)
     #print(p1)
 
