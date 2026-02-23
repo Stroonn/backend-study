8a6b5b2953c0fb6d6065cffd2a53193b4ef28be1
Stroonn <matheus.gato.gt98@gmail.com>
2026-02-23 19:51:45 -0300

    refactor main function to initialize ProductService with repository

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from zipfile import Path
+from pathlib import Path
 from app.repositories.product_repository import ProductRepository
 from app.services.user_services import UserService
 from app.services.product_services import ProductService
@@ -8,27 +8,19 @@ from app.models.product import Product
 from app.errors.product_errors import ProductError
 
 def main():
-    #service = UserService()
-    #u1 = service.create_user("Matheus", "matheus@gmail.com")
-    #print(u1)
-    #u2 =  service.create_user("Gabriela", "gabriela@gmail.com")
     repo = ProductRepository(Path("data/products.json"))
-    p_service = ProductService()
-    try:
-        product = p_service.create_product("Plate", "Ceramic plate", 27, 45)
-        product.price = format_price(product.price)
-        print(product)
-    except ProductError as e:
-        print(f"Error: {e}")
-
-    #u1 = User("Matheus", "matheus@gmail.com")
-    #u2 = User("Matheus", "matheus@gmail.com")
-
-    #print(u1 == u2)
-
-    #p1 = Product("Glass", "Blue glass", 10, 50)
-    #p1.price = format_price(p1.price)
-    #print(p1)
+    p_service = ProductService(repo)
+    #try:
+    #    product = p_service.create_product("Plate", "Ceramic plate", 27, 45)
+    #    product.price = format_price(product.price)
+    #    #print(product)
+    #except ProductError as e:
+    #    print(f"Error: {e}")
+    #p_service.update_price("Plate", 105)
+    #print(p_service.list_products())
+    #p_service.delete_product("Plate")
+    #print(p_service.list_products())
+    print(p_service.find_by_name("Glass"))
 
 if __name__ == "__main__":
     main()
