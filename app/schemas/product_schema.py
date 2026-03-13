f08c5258ef6be3cef25fc9f8568a01d6631356cb
Stroonn <matheus.gato.gt98@gmail.com>
2026-03-13 19:40:12 -0300

    feat: enhance product validation and pydantic constraints

--- app/schemas/product_schema.py
+++ app/schemas/product_schema.py
@@ -1,4 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+class ProductCreateSchema(BaseModel):
+    name: str
+    desc: str
+    amount: int = Field(ge=0)
+    price: float = Field(gt=0)
 
 class ProductResponseSchema(BaseModel):
     id: int
