76c98825a3fcfa2ac525214589b8207a0414b487
Stroonn <matheus.gato.gt98@gmail.com>
2026-03-17 17:55:31 -0300

    test: improve test setup using reusable fixtures

---
+++ test/conftest.py
@@ -0,0 +1,33 @@
+import sqlite3
+import pytest
+from fastapi.testclient import TestClient
+from main_api import app, get_service
+from app.repositories.product_sql_repository import ProductSQLiteRepository
+from app.services.product_services import ProductService
+
+@pytest.fixture
+def client():
+
+    connection = sqlite3.connect(":memory:", check_same_thread=False)
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    CREATE TABLE products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        desc TEXT,
+        amount INTEGER,
+        price REAL
+    )
+    """)
+    connection.commit()
+
+    repository = ProductSQLiteRepository(connection)
+    service = ProductService(repository)
+
+    def override_get_service():
+        return service
+
+    app.dependency_overrides[get_service] = override_get_service
+
+    return TestClient(app)
